581c7e84e5acf0a76aa934f89a279804a4230651
Thai Do <thai.hl.do@gmail.com>
2020-08-18 11:39:29 +0000

    Refactored version of loopy-lighthouse code

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,12 +1,29 @@
-let stop = 200;
-for(let x = 100; x <= stop; x++) {
-  if(x % 3 === 0 && x % 4 === 0) {
-    console.log("LoopyLighthouse");
-  } else if(x % 3 === 0) {
-    console.log("Loopy");
-  } else if(x % 4 === 0) {
-    console.log("Lighthouse");
-  } else {
-    console.log(x);
+//let stop = 200;
+//for(let x = 100; x <= stop; x++) {
+//  if(x % 3 === 0 && x % 4 === 0) {
+//    console.log("LoopyLighthouse");
+//  } else if(x % 3 === 0) {
+//    console.log("Loopy");
+//  } else if(x % 4 === 0) {
+//    console.log("Lighthouse");
+//  } else {
+//    console.log(x);
+//  }
+//}
+
+
+// Refactored Version 
+
+for (const num of nums) {
+  let output = "";
+
+  if (num % 3 === 0) {
+    output += "Loopy";
   }
+
+  if (num % 4 === 0) {
+    output += "Lighthouse";
+  }
+
+  console.log(output === "" ? num : output);
 }
